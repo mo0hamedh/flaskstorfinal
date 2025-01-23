41c1eb2a5c96e9de42b9ff37f0ce42cd1d440161
mo0hamedh <mohamedhosamopp122@icloud.com>
2025-01-24 01:43:59 +0200

    user class : email address

--- market/models.py
+++ market/models.py
@@ -3,6 +3,8 @@ from market import db
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
