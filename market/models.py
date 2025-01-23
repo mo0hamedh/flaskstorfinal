d83115feb6a07a84a3f1f42b55c14a295279391f
mo0hamedh <mohamedhosamopp122@icloud.com>
2025-01-24 01:45:35 +0200

    user class : password

--- market/models.py
+++ market/models.py
@@ -4,6 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
 
 
 class Item(db.Model):
