2da73486fbe50efed0f5b03f4db852ede5d2d9af
mo0hamedh <mohamedhosamopp122@icloud.com>
2025-01-25 00:26:27 +0200

    login logout

--- market/routes.py
+++ market/routes.py
@@ -25,6 +25,8 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
@@ -53,12 +55,5 @@ def logout_page():
     logout_user()
     flash("You have been logged out!", category='info')
     return redirect(url_for("home_page"))
-
-
-
-
-
-
-
 
 
