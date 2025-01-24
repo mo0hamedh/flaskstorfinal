4744a829186f0f2c3d074fbebb89b330ede321d0
mo0hamedh <mohamedhosamopp122@icloud.com>
2025-01-24 23:48:04 +0200

    login view

--- market/routes.py
+++ market/routes.py
@@ -11,6 +11,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required()
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
