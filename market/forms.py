9b57885c2c7e7ff7efc42b4a0c6155fd60d18835
mo0hamedh <mohamedhosamopp122@icloud.com>
2025-01-24 08:52:57 +0200

    validate_email_address

--- market/forms.py
+++ market/forms.py
@@ -9,6 +9,11 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(username=username_to_check.data).first()
         if user:
             raise ValidationError('Username already exists! Please try a different username')
+    
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
         
     
 
